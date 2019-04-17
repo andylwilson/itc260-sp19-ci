99f80a5005e87ad59da96a9d6073648119add19b
Andy <38418756+andylwilson@users.noreply.github.com>
2019-04-16 18:49:58 -0700

    added theme for news, view, and create

---
+++ sp19-2/application/config/custom_config.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+//custom_config.php - a place to save global settings
+
+$config['style'] = 'simplex.css';
+$config['banner'] = 'The Pine St Journal';
+$config['title'] = 'Welcome to the news of the northwest!';
+$config['copyright'] = 'Default Copyright';
+$config['masthead'] = 'Default MastHead';
+$config['theme'] = 'themes/bootswatch/';
--- sp19-2/application/views/news/create.php
+++ sp19-2/application/views/news/create.php
@@ -1,7 +1,9 @@
 <?php
 //application/views/news/create.php
-?>
 
+$this->load->view($this->config->item('theme') . 'header');
+
+?>
 <h2><?php echo $title; ?></h2>
 
 <?php echo validation_errors(); ?>
@@ -17,3 +19,7 @@
     <input type="submit" name="submit" value="Create news item" />
 
 </form>
+
+<?php
+$this->load->view($this->config->item('theme') . 'footer');
+?>
